8fe2b32ab168ca014e6ac6cfa94ed5d8ae1f180d
Alexander Pustovoy <aspustovoy@gmail.com>
2024-07-29 15:46:27 +0300

    added the layout

---
+++ src/js/_vars.js
@@ -0,0 +1,9 @@
+export default {
+  $windowEl: window,
+  $documentEl: document,
+  $htmlEl: document.documentElement,
+  $bodyEl: document.body,
+  // $burger: document.querySelector('.burger'),
+  // $nav: document.querySelector('.nav'),
+}
+
---
+++ src/js/components/hero-btn.js
@@ -0,0 +1,12 @@
+// const btns = document.querySelectorAll('.hero__button');
+// const banner = document.querySelector('.banner');
+
+// if (btns) {
+//   const bannerOn = () => {
+//     banner.classList.add('banner--active');
+//   };
+
+//   btns.forEach((btn) => {
+//     btn.onclick = bannerOn;
+//   });
+// }
